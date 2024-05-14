bd2b6bc4738d588a9bafe936fca4422f09a1baf6
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-05-14 19:34:33 +0100

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>May 14,2024</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$650</div>
+            </div>
+        </div>)
+}
+
+export default ExpenseItem;

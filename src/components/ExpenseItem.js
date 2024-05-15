15e70d4b49c45fd58ec3d4081bda328c2556b4ee
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-05-15 11:32:26 +0100

    styled the date section

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,19 @@
 
+
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
-    const month = props.date.toLocaleString('en-Us', { month: 'long'});
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString('en-Us', { day: 'numeric'})
     return (
         <div className="expense-item">
-            <div>
-                <div>{year}</div>
-                <div>{month}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>)
+        </div>
+    );
 }
 
 export default ExpenseItem;

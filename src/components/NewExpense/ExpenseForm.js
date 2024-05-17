451101fb84caf28836d2f07aa7b22e5f6cf7e4c4
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-05-18 00:13:32 +0100

    expense form complete

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,24 +6,31 @@ import './ExpenseForm.css'
 const ExpenseForm = () => {
 
     return (
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type='text'/>
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type='text' />
+                </div>
+
+                <div className="new-expense__control">
+                    <label>Amout</label>
+                    <input type='number' min='0.05' step='0.05' />
+                </div>
+
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type='date' min='2024-05-18' max='2026-11-15' />
+                </div>
+
             </div>
 
-            <div className="new-expense__control">
-                <label>Amout</label>
-                <input type='number' min='0.05' step='0.05'/>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
             </div>
 
-            <div className="new-expense__control">
-                <label>Date</label>
-                <input type='text'/>
-            </div>
+        </form>
 
-
-        </div>
     )
 }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,13 +1,14 @@
 
 
 import React from "react";
-import './NewExpense.css'
+import './NewExpenses.css'
+import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
 
     return (
         <div className="new-expense">
-            
+            <ExpenseForm />
         </div>
     )
 

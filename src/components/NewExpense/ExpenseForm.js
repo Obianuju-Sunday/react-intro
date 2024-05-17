c3a27daaf3aa39838f6c504174d0856b5f0c31d1
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-05-17 23:59:09 +0100

    expenseform

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,30 @@
+
+
+import React from "react";
+import './ExpenseForm.css'
+
 const ExpenseForm = () => {
 
+    return (
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type='text'/>
+            </div>
+
+            <div className="new-expense__control">
+                <label>Amout</label>
+                <input type='number' min='0.05' step='0.05'/>
+            </div>
+
+            <div className="new-expense__control">
+                <label>Date</label>
+                <input type='text'/>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default ExpenseForm;

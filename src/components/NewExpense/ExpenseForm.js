606df0a8796ca85265404633c49669520d419491
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-05-20 19:13:02 +0100

    gathered user inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,54 @@
 
 
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
 
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    } 
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        console.log(expenseData);
+    }
+
+
+    console.log(titleChangeHandler, amountChangeHandler, dateChangeHandler);
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onChange={titleChangeHandler}/>
                 </div>
 
                 <div className="new-expense__control">
-                    <label>Amout</label>
-                    <input type='number' min='0.05' step='0.05' />
+                    <label>Amount</label>
+                    <input type='number' min='0.05' step='0.05' onChange={amountChangeHandler}/>
                 </div>
 
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min='2024-05-18' max='2026-11-15' />
+                    <input type='date' min='2024-05-18' max='2026-11-15' onChange={dateChangeHandler}/>
                 </div>
 
             </div>
@@ -35,3 +63,17 @@ const ExpenseForm = () => {
 }
 
 export default ExpenseForm;
+
+
+
+// Keep in mind to use if your state update depends on the previous state // 
+
+// const [userInput, setUserIput] = useState({
+//     enteredAmount: '',
+//     enteredDate: '',
+//     enteredTitle: '',
+// })
+
+// setUserIput((prevState) => {
+//     return {...prevState, enteredTitle: event.target.value}
+// });

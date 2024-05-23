f3849ea03f469b9813853ef2bbffc628ee99664b
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-05-23 19:50:39 +0100

    child-parent component communication

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,10 +5,17 @@ import './NewExpenses.css'
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        console.log(expenseData)
+    }
 
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
         </div>
     )
 

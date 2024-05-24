c8e997b2d46b565c4695c1ee8f960a9418a04e33
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-05-24 14:23:23 +0100

    dynamic listing

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -14,6 +14,8 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+
+
         </Card>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -23,6 +23,7 @@ const Expenses = (props) => {
                 <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
             ))}
 
+
         </Card>
     );
 }
